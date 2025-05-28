212d8d5d481adaf74ee8bba9ef584efd399fe374
henriMacedoGoncalves <henri.macedogoncalves@gmail.com>
2025-05-28 21:33:15 +0200

    Carousel Image added - import coreui - modified introduction component - angular updated to 19.2.14

--- src/app/components/introduction/introduction.component.ts
+++ src/app/components/introduction/introduction.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import {
+  CarouselCaptionComponent,
+  CarouselComponent,
+  CarouselControlComponent,
+  CarouselIndicatorsComponent,
+  CarouselInnerComponent,
+  CarouselItemComponent
+} from '@coreui/angular';
 
 @Component({
   selector: 'app-introduction',
   standalone: true,
-  imports: [],
+  imports: [
+    CarouselComponent,
+    CarouselIndicatorsComponent,
+    CarouselInnerComponent,
+    CarouselItemComponent,
+    CarouselCaptionComponent,
+    CarouselControlComponent,
+    RouterLink,
+    CommonModule
+  ],
   templateUrl: './introduction.component.html',
   styleUrl: './introduction.component.css'
 })
-export class IntroductionComponent {
+export class IntroductionComponent implements OnInit {
+  slides: any[] = new Array(3).fill({ id: -1, src: '', title: '', subtitle: '' });
 
+  ngOnInit(): void {
+    this.slides[0] = {
+      id: 0,
+      src: 'assets/images/static/placeholder.png',
+      title: 'First slide',
+      subtitle: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
+    };
+    this.slides[1] = {
+      id: 1,
+      src: 'assets/images/static/placeholder.png',
+      title: 'Second slide',
+      subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    };
+    this.slides[2] = {
+      id: 2,
+      src: 'assets/images/static/placeholder.png',
+      title: 'Third slide',
+      subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
+    };
+  }
 }
