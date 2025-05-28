6f89a7a18446f945c11a7f0009f102793c325e85
henriMacedoGoncalves <henri.macedogoncalves@gmail.com>
2025-05-28 18:25:21 +0200

    Initial structure of the frontend - web icon added to the tab - list of components:     - login status     - community     - login     - overview     - profile     - cart     - onlinecoaching     - login

---
+++ src/app/components/onlinecoaching/onlinecoaching.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-onlinecoaching',
+  imports: [],
+  templateUrl: './onlinecoaching.component.html',
+  styleUrl: './onlinecoaching.component.css'
+})
+export class OnlinecoachingComponent {
+
+}
