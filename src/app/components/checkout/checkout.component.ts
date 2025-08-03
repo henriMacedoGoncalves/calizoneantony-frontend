308e88ab21f465e043728b1ee2674d52c4355547
henriMacedoGoncalves <henri.macedogoncalves@gmail.com>
2025-08-03 16:10:44 +0200

    Verify if right input

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -12,6 +12,7 @@ import { CommonModule } from '@angular/common';
 import { CaliZoneAntonyService } from '../../services/cali-zone-antony.service';
 import { State } from '../../common/state';
 import { Country } from '../../common/country';
+import { Order } from '../../common/order';
 
 @Component({
   selector: 'app-checkout',
@@ -41,32 +42,44 @@ export class CheckoutComponent implements OnInit {
         firstName: new FormControl('', [
           Validators.required,
           Validators.minLength(2),
+          this.notOnlyWhitespace,
         ]),
         lastName: new FormControl('', [
           Validators.required,
           Validators.minLength(2),
+          this.notOnlyWhitespace,
         ]),
         email: new FormControl('', [
           Validators.required,
-          Validators.minLength(2),
+          // ^$ finisher
+          // [ accepted terms ]
+          // + concatenate
+          // outside [] must terms
+          // \\ escaped terms
+          // {min,max} terms
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
         ]),
       }),
       billingAddress: this.formBuilder.group({
         street: new FormControl('', [
           Validators.required,
           Validators.minLength(2),
+          this.notOnlyWhitespace,
         ]),
         city: new FormControl('', [
           Validators.required,
           Validators.minLength(2),
+          this.notOnlyWhitespace,
         ]),
         state: new FormControl('', [Validators.required]),
         country: new FormControl('', [Validators.required]),
         zipCode: new FormControl('', [
           Validators.required,
           Validators.minLength(2),
+          this.notOnlyWhitespace,
         ]),
       }),
+      creditCard: this.formBuilder.group({}),
     });
 
     this.caliZoneAntonyService.getCountries().subscribe((data) => {
@@ -81,22 +94,55 @@ export class CheckoutComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.checkoutForm);
+    if (this.checkoutForm.invalid) {
+      this.checkoutForm.markAllAsTouched();
+      return;
+
+      let order = new Order();
+    }
   }
 
   getStates(formGroupName: string) {
     const formGroup = this.checkoutForm.get(formGroupName);
 
     const countryCode = formGroup?.value.country.code;
-    const countryName = formGroup?.value.country.title;
-
-    console.log(`${formGroupName} country code: ${countryCode}`);
-    console.log(`${formGroupName} country name: ${countryName}`);
 
     this.caliZoneAntonyService.getStates(countryCode).subscribe((data) => {
       this.billingAddressStates = data;
 
       formGroup!.get('state')?.setValue(data[0]);
     });
+  }
+
+  get firstName() {
+    return this.checkoutForm.get('user.firstName');
+  }
+  get lastName() {
+    return this.checkoutForm.get('user.lastName');
+  }
+  get email() {
+    return this.checkoutForm.get('user.email');
+  }
+  get country() {
+    return this.checkoutForm.get('user.country');
+  }
+  get state() {
+    return this.checkoutForm.get('user.state');
+  }
+  get street() {
+    return this.checkoutForm.get('user.street');
+  }
+  get zipCode() {
+    return this.checkoutForm.get('user.zipCode');
+  }
+
+  notOnlyWhitespace(control: FormControl): ValidationErrors | null {
+    if (control.value != null && control.value.trim().length === 0) {
+      //invalid, return error object
+      return { notOnlyWhitespace: true };
+    } else {
+      // valid, return null
+      return null;
+    }
   }
 }
